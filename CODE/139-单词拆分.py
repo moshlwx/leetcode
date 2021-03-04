5070eaea6fcb41ab1edbb441c14228adca4a7ae6
李文学 00465431 <liwenxue2@huawei.com>
2021-03-04 20:11:34 +0800

    完成单词拆分的bfs\dfs\dp解法

--- CODE/139-单词拆分.py
+++ CODE/139-单词拆分.py
@@ -25,51 +25,75 @@
 
 
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        '''遍历s，匹配wordDict，若存在，则继续。打断后回退到上一层
-        遍历wordDict，寻找s中的路径
+    def wordBreak_dfs(self, s: str, wordDict) -> bool:
+        wordSet = set(wordDict)
 
+        # visited用数组或者set按照下标访问，用字典复杂度会超
+        visted = [-1 for _ in range(len(s))]
+
+        def can_break(start):
+            if start == len(s):
+                return True
+            if visted[start] != -1:
+                return visted[start]
+
+            for i in range(start+1, len(s)+1):
+                prefix = s[start: i]
+                if prefix in wordSet and can_break(i):
+                    visted[start] = True
+                    return True
+            visted[start] = False
+            return False
+
+        return can_break(0)
+
+    def wordBreak_bfs(self, s: str, wordDict) -> bool:
+        from queue import Queue
 
         wordSet = set(wordDict)
-        from queue import Queue 
         q = Queue()
+        q.put(0)
         visted = set()
 
-        q.put(wordDict[0])
-
         while not q.empty():
-            q_size = q.qsize()
+            cur = q.get()
+            if cur in visted:
+                continue
+            visted.add(cur)
 
-            for _ in range(qsize):
-                cur = q.get()
-            left += 1
+            for i in range(cur+1, len(s)+1):
+                prefix = s[cur: i]
 
-        return True if 
+                if prefix in wordSet:
+                    # 子串在wordSet中，若i未到边界，继续判断cur_start到i的子串
+                    if i < len(s):
+                        q.put(i)
+                    # 若i到边界，返回真
+                    else:
+                        return True
+
+        return False
+
+    def wordBreak(self, s: str, wordDict) -> bool:
+        '''动态规划
+        dp[i][j] 前j个字符串是否满足
+        状态转移：dp[i][j] = dp[i-1][j-1] and item[j: j+1] in wordSet or dp[i-1][j-k] and item[j-k: j+1] in wordSet
         '''
+        wordSet = set(wordDict)
+        dp = [False for _ in range(len(s)+1)]
+        dp[0] = True
 
-        '''DFS多叉树遍历
-        
-        '''
-        def backtrack(path, choose):
-            '''
-            输入路径、选择
-            当len(path) == len(s) 成功
-            '''
-            if len(path) == len(s):
-                return True
+        for j in range(1, len(s)+1):
+            for k in range(j, 0, -1):
+                dp[j] = dp[j] or (dp[j-k] and s[j-k: j] in wordSet)
+            
+        return dp[-1]
 
-            if path in wordSet:
-                backtrack(path, choose)
-            else:
-                return
 
-            for c in choose:
-                path.append(c)
-                backtrack(path, choose)
-                path.pop()
+s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
+wordDict = ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa",
+            "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]
+s = "leetcode"
+wordDict = ["leet", "code"]
 
-        res = False
-
-        backtrack([], choose)
-
-        return res
+print(Solution().wordBreak(s, wordDict))
