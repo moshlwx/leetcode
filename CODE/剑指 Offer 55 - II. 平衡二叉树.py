849df00bbd02ed2c298426424e49e4339e4adea5
李文学 00465431 <liwenxue2@huawei.com>
2021-04-20 20:36:22 +0800

    补充readme中的题目

--- CODE/剑指 Offer 55 - II. 平衡二叉树.py
+++ CODE/剑指 Offer 55 - II. 平衡二叉树.py
@@ -37,11 +37,11 @@ r'''
 '''
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
