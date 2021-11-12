8e78645b622385bba56de62c43c5ac74ef86c083
moshlwx <1298076610@qq.com>
2021-11-12 12:16:50 +0800

    剑指 Offer II 007. 数组中和为 0 的三个数 双指针+跳过重复元素的方法解决

--- CODE/剑指 Offer II 007. 数组中和为 0 的三个数.py
+++ CODE/剑指 Offer II 007. 数组中和为 0 的三个数.py
@@ -31,6 +31,41 @@
 class Solution:
     def threeSum(self, nums):
         '''数组的大小1000，应该接受O(nlogn)~O(n^2)级别的复杂度
+        双指针方法，先排序O(n) + O(n^2)
+        优化点 判断重复元素直接跳过，现在用set去重，略慢，但是能通过
+        '''
+        res = []
+
+        nums = sorted(nums)
+
+        for i, ni in enumerate(nums):
+            if i > 0 and ni == nums[i-1]:
+                continue
+
+            left = i+1  # 从i开始寻找
+            right = len(nums) - 1
+
+            while left < right:
+                if nums[left] + nums[right] > -ni:
+                    right -= 1
+                elif nums[left] + nums[right] < -ni:
+                    left += 1
+                else:
+                    if left != i and right != i:
+                        res.append([ni, nums[left], nums[right]])
+                    right -= 1
+                    left += 1
+                    # 当出现重复元素时，跳过，注意是在sum==0的条件中跳过，
+                    # 在外面循环中跳过会额外跳过没计算过sum的元素
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+
+        return res
+
+    def threeSum_Counter(self, nums):
+        '''数组的大小1000，应该接受O(nlogn)~O(n^2)级别的复杂度
         哈希表方法，用暴力O(n) + O(n^2)
         '''
         res = set()
@@ -53,6 +88,7 @@ class Solution:
 
 
 # 输入：
-nums = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
+nums = [-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]
 # 输出：[[-1,-1,2],[-1,0,1]]
 print(Solution().threeSum(nums))
+print(Solution().threeSum_Counter(nums))
